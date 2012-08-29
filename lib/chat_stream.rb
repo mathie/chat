076ec26b5c07f3212c0b4085987a3ac5ae1ce62a
Graeme Mathieson <mathie@woss.name>
2012-08-29 11:53:02 +0100

    Tidy up and set the response content type.

--- lib/chat_stream.rb
+++ lib/chat_stream.rb
@@ -5,23 +5,16 @@ class ChatStream < Sinatra::Base
     enable :logging
   end
 
-  get '/stream' do
+  get '/stream', provides: 'text/event-stream' do
     stream(:keep_open) do |out|
       timer = EventMachine::PeriodicTimer.new(1) do
-        logger.info "Sending some data"
         data = {
           timestamp: Time.now.utc
         }
         out << "data: #{data.to_json}\n\n"
       end
 
-      out.errback do
-        logger.info "Got an errback for"
-        timer.cancel
-      end
-
       out.callback do
-        logger.info "Callback for out"
         timer.cancel
       end
     end
