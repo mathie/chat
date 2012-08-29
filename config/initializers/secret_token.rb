70a418fdcd3bf0743e3a0ffee76405e894e513bc
Graeme Mathieson <mathie@woss.name>
2012-08-29 08:57:09 +0100

    Bare rails skeleton.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chat::Application.config.secret_token = '1c22e83b9bb6e0bf30670541e7799c57df04176d8082ba07fd948b775298e6ffe4d71800f803bc0226f81e8f3f91df716e27e30a4cb4a2767946d573a4e61c6a'
