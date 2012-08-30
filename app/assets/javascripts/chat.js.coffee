8e61a48d1cb9972f3cc5ec3e1aeb84e7ab8a94e2
Graeme Mathieson <mathie@woss.name>
2012-08-30 16:49:30 +0100

    Tidy up and figure out the latency on data transmission.

--- app/assets/javascripts/chat.js.coffee
+++ app/assets/javascripts/chat.js.coffee
@@ -1,19 +1,37 @@
-class Timestamp extends Backbone.Model
+window.Chat =
+  Models: {}
+  Views: {}
+
+class Chat.Models.Base extends Backbone.Model
+  get: (attr) ->
+    value = super
+    if _.isFunction(value) then value.call(@) else value
+
+class Chat.Models.Timestamp extends Chat.Models.Base
   defaults:
     serverTime: 'Unknown'
+    clientToSinatraLatency: 0
+    sinatraToBrowserLatency: 0
+    clientToBrowserLatency: ->
+      console.log "Called with #{@}"
+      @get('clientToSinatraLatency') + @get('sinatraToBrowserLatency')
 
   initialize: ->
     $.eventsource
-      label: 'current-time'
+      label: 'timestamp'
       dataType: 'json'
-      url: '/chat/stream'
+      url: '/chat/timestamp'
       message: @messageReceived
 
   messageReceived: (data) =>
     serverTime = new Date data.timestamp
+    clientToSinatraLatency = data.latency
+    sinatraToBrowserLatency = (new Date).getTime() - data.milliseconds
     @set('serverTime', serverTime)
+    @set('clientToSinatraLatency', clientToSinatraLatency)
+    @set('sinatraToBrowserLatency', sinatraToBrowserLatency)
 
-class TimestampView extends Backbone.View
+class Chat.Views.TimestampView extends Backbone.View
   tagName:   'p'
   className: 'pull-right navbar-text'
 
@@ -23,10 +41,10 @@ class TimestampView extends Backbone.View
     @model.bind 'change', @render
 
   render: =>
-    $(@el).html(@template(serverTime: @model.get('serverTime')))
+    $(@el).html(@template(model: @model))
     @
 
 $ ->
-  timestamp = new Timestamp
-  timestampView = new TimestampView(model: timestamp)
+  timestamp = new Chat.Models.Timestamp
+  timestampView = new Chat.Views.TimestampView(model: timestamp)
   $('#timestamp-container').html(timestampView.render().el)
--- lib/chat_stream.rb
+++ lib/chat_stream.rb
@@ -3,47 +3,33 @@ require 'sinatra/base'
 $active_streams = 0
 
 class ChatStream < Sinatra::Base
-  def initialize(app = nil)
-    super
-
-    configure_amqp_queue
-  end
+  set connection: -> { AMQP.connect(host: '127.0.0.1') }
 
   configure do
     enable :logging
   end
 
-  get '/stream', provides: 'text/event-stream' do
-    stream_number = $active_streams
-    $active_streams += 1
-    logger.info "GET /stream start [#{stream_number}]"
+  get '/timestamp', provides: 'text/event-stream' do
     stream(:keep_open) do |out|
-      logger.info "[#{stream_number}] Stream opened."
-      AMQP::Channel.new(@connection, auto_recovery: true) do |channel|
-        logger.info "[#{stream_number}] Channel opened."
-        channel.queue('', durable: false, auto_delete: true).bind('chat.stream').subscribe do |metadata, payload|
-          logger.info "Payload received on stream #{stream_number}."
+      AMQP::Channel.new(settings.connection, auto_recovery: true) do |channel|
+        channel.queue('', durable: false, auto_delete: true).bind('chat.stream').subscribe do |metadata, payload_json|
+          payload = JSON.parse(payload_json)
+          milliseconds = Time.now.utc.to_f * 1000.0
           data = {
             timestamp: Time.now.utc,
+            milliseconds: milliseconds,
+            latency: milliseconds - payload['milliseconds'],
             metadata: metadata,
-            payload: JSON.parse(payload)
+            payload: payload
           }
           out << "event: #{metadata.type}\n"
           out << "data: #{data.to_json}\n\n"
         end
 
         out.callback do
-          $active_streams -= 1
-          logger.info "Callback called, stream closed. channel #{channel.open? ? "open" : "closed"}. Active streams: #{$active_streams}. [#{stream_number}]"
           channel.close if channel.open?
         end
       end
     end
-    logger.info "GET /stream finished [#{stream_number}]"
-  end
-
-  private
-  def configure_amqp_queue
-    @connection = AMQP.connect(host: '127.0.0.1')
   end
 end
